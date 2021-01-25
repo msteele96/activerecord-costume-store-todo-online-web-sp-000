e06ef5116c5dcb757ab095f1234f4bb8d53e8b71
Marc Steele <marcsteele2014@gmail.com>
2021-01-25 18:52:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStore < ActiveRecord::Migration[5.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.string :opening_time
+      t.string :closing_time
     end
   end
 
